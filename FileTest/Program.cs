224a36397cbf6274e376684bb72de812d6958d9b
Sundaday <john.koch.pro@gmail.com>
2022-03-13 18:16:58 +0100

    add directory

--- FileTest/Program.cs
+++ FileTest/Program.cs
@@ -1,5 +1,19 @@
-﻿//Init path 
+﻿//Init path => MyDocuments
+//var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+//Create folder "testTest"
+var path = "testTest";
+Directory.CreateDirectory(path);
+
+
+//Init name of our txt
 string fileName = "FileTest.txt";
+
+//Combine path & file for a full compatibility over systems
+string pathAndFile = Path.Combine(path, fileName);
+
+//Show path
+Console.WriteLine("We are here => " + pathAndFile);
+Console.WriteLine();
 
 //Anonymous list
 var names = new List<string>()
@@ -13,10 +27,10 @@ var names = new List<string>()
 File.WriteAllText("FichierTest.txt", "c'est ce que j'ai écris dans mon fichier");
 
 //Create file, write then close; Write after if not empty
-File.AppendAllText(fileName, "j'ai add ceci");
+File.AppendAllText(pathAndFile, "j'ai add ceci");
 
 //Open pathfile(fileName), write(names) then close
-File.WriteAllLines(fileName, names);
+File.WriteAllLines(pathAndFile, names);
 
 try
 {
@@ -24,7 +38,7 @@ try
     //string result = File.ReadAllText(fileName);
 
     //Read file then return it in array 
-    var result = File.ReadAllLines(fileName);
+    var result = File.ReadAllLines(pathAndFile);
     foreach(var line in result)
     {
         Console.WriteLine(line);
@@ -35,4 +49,4 @@ catch (FileNotFoundException ex)
     Console.WriteLine(ex.ToString());
 }
 
-File.Delete(fileName);
+//File.Delete(pathAndFile);
